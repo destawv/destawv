29b9674bb7bb7fd14a99ee9ac7cd88d7a9ab59fa
desta <cmwani99@gmail.com>
2026-08-01 00:32:12 +0700

    nodejs server

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'index.html');
+  
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error loading page');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(content);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
